598a2e9f3928ba2ca33c53ecc2df0a24a4a83af5
Matt Wilson <mattwwilson34@gmail.com>
2022-03-26 14:41:22 -0400

    Added Section component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,19 @@
-import './Home.css'
+import './Home.css';
 import React from 'react';
+import Section from '../../Components/Section';
 
 const Home = () => {
-  return <div>Home COMPONENT</div>;
-  }; 
+  return (
+    <div className='Home'>
+      <Section
+        className='Home__Hero__Section'
+        height='90vh'
+        width='100vw'
+        color='rgb(40, 44, 52)'>
+        <div>HERO</div>
+      </Section>
+    </div>
+  );
+};
 
-  export default Home;
+export default Home;
