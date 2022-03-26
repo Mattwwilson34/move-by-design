b416d59e590987249caa22fd7731a1ed4257fd73
Matt Wilson <mattwwilson34@gmail.com>
2022-03-25 21:30:51 -0400

    Initial commit

---
+++ src/Components/Section/Section.jsx
@@ -0,0 +1,8 @@
+import './Section.css'
+import React from 'react';
+
+const Section = () => {
+  return <div>Section COMPONENT</div>;
+  }; 
+
+  export default Section;
---
+++ src/Components/Section/__test__/Section.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Section from '../Section';
+
+describe('Section component', () => {
+  it('Does something...', () => {
+
+  });
+});
