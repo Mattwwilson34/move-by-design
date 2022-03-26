9722cc8ecc8c4e371a1de6b6a284b33927f651c1
Matt Wilson <mattwwilson34@gmail.com>
2022-03-25 21:05:57 -0400

    Added primary NavLinks

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,8 +1,24 @@
-import './Nav.css'
+import './Nav.css';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
-  return <div>Nav COMPONENT</div>;
-  }; 
+  return (
+    <div>
+      <NavLink className='Nav__Link' to='about'>
+        About
+      </NavLink>
+      <NavLink className='Nav__Link' to='contact'>
+        Contact
+      </NavLink>
+      <NavLink className='Nav__Link' to='Testimonials'>
+        Testimonials
+      </NavLink>
+      <NavLink className='Nav__Link' to='Movement-Archive'>
+        Movement Archive
+      </NavLink>
+    </div>
+  );
+};
 
-  export default Nav;
+export default Nav;
