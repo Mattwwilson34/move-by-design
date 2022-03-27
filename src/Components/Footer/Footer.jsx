d0b277b327bdbabb49a0066bc132ea45b9ec0607
Matt Wilson <mattwwilson34@gmail.com>
2022-03-26 22:33:07 -0400

    Initial commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,19 @@
-import './Footer.css'
+import './Footer.css';
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer COMPONENT</div>;
-  }; 
+  return (
+    <div className='Footer'>
+      <div className='Footer__Container'>
+        <span>Move By Design, LLC</span>
+        <span>Durham, NC</span>
+        <a href='mailto:culver.dean.coach@gmail.com'>
+          culver.dean.coach@gmail.com
+        </a>
+        <span>est. 2021</span>
+      </div>
+    </div>
+  );
+};
 
-  export default Footer;
+export default Footer;
