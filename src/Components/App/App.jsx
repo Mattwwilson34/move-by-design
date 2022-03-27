edb35c18c2c6195b243878d274ee4a7c19746210
Matt Wilson <mattwwilson34@gmail.com>
2022-03-27 11:32:39 -0400

    Added Footer component

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from '../Nav';
+import Footer from '../Footer';
 
 const App = () => {
   return (
     <div>
       <Nav />
       <Outlet />
+      <Footer />
     </div>
   );
 };
